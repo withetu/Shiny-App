c741208e6ef9c5618fb95a7610e87714f0e96c1e
Sandip Paul <sandeep.paul84@gmail.com>
2017-01-12 16:57:37 +0530

    Building Shiny App exercises part 3
    http://r-exercises.com/2016/12/25/building-shiny-app-exercises-part-3/

--- ui.R
+++ ui.R
@@ -21,7 +21,7 @@
 #To create a header element: select a header function e.g. (h1) and give it the text you want to see in the header.
 #For example, you can create a first level header that says “Title” with h1("Title").
 #To place the element in your app:
-  #Put h1("Title") as an argument to titlePanel(), sidebarPanel(), or mainPanel().
+#Put h1("Title") as an argument to titlePanel(), sidebarPanel(), or mainPanel().
 #The text will appear in the corresponding panel of your web page. You can place multiple elements in the same panel if you separate them with a comma.
 #Create an HTML element to add the title “Menu” in the sidebarPanel() and “Main” in mainPanel() with one of Shiny’s tag functions. HINT: Use h1,h2.
 
@@ -81,15 +81,15 @@
 
 #BUTTONS
 #In the example below we create a UI with a submitButton and an actionButton. Please note that we use the function fluidrow to make sure that all the elements we are going to use will be in the same line as we are going to need this in the next parts:
-  # ui.R
-  #shinyUI(fluidPage(
-    #titlePanel(“Widgets”),
-    
-    #fluidRow(h3(“Buttons”),
-             #actionButton(“action”, label = “Action”),
-             #br(),
-             #br(),
-             #submitButton(“Submit”))))
+# ui.R
+#shinyUI(fluidPage(
+#titlePanel(“Widgets”),
+
+#fluidRow(h3(“Buttons”),
+#actionButton(“action”, label = “Action”),
+#br(),
+#br(),
+#submitButton(“Submit”))))
 
 # server.R
 #shinyServer(function(input, output) {
@@ -101,12 +101,12 @@
 
 #SINGLE CHECKBOX
 #In the example below we create a UI with a single Checkbox:
-  # ui.R
-  #shinyUI(fluidPage(
-    #titlePanel(“Widgets”),
-    
-    #fluidRow(h3(“Single checkbox”),
-             #checkboxInput(“checkbox”, label = “Choice A”, value = TRUE))))
+# ui.R
+#shinyUI(fluidPage(
+#titlePanel(“Widgets”),
+
+#fluidRow(h3(“Single checkbox”),
+#checkboxInput(“checkbox”, label = “Choice A”, value = TRUE))))
 
 #server.R
 #shinyServer(function(input, output) {
@@ -118,10 +118,10 @@
 
 #CHECKBOX GROUP
 #In the example below we create a UI with a Checkbox Group:
-  #ui.R
-  #shinyUI(fluidPage(
-    #checkboxGroupInput(“checkGroup”,
-                        #label = h3(“Checkbox group”),
+#ui.R
+#shinyUI(fluidPage(
+#checkboxGroupInput(“checkGroup”,
+#label = h3(“Checkbox group”),
 #choices = list(“Choice 1” = 1,
 #“Choice 2” = 2, “Choice 3” = 3),
 #selected = 2)
@@ -136,7 +136,7 @@
 
 #DATE INPUT
 #In the example below we create a UI with a Date Input:
-  #ui.R
+#ui.R
 #shinyUI(fluidPage(
 #dateInput(“date”,
 #label = h3(“Date input”),
@@ -151,7 +151,7 @@
 
 #DATE RANGE
 #In the example below we create a UI with a Date Range Input:
-  #ui.R
+#ui.R
 #shinyUI(fluidPage(
 #dateRangeInput(“dates”, label = h3(“Date range”))
 #))
@@ -258,74 +258,90 @@
 
 #Replace the value with a default range “10-90” and see the difference.
 
+#TEXT INPUT
+#In the example below we create a UI with a textInput.
+# ui.R
+#shinyUI(fluidPage(
+#titlePanel("Widgets"),
+#textInput("text", label = h3("Text Input"),
+#value = "Text..."))
+#)
+#server.R
+#shinyServer(function(input, output) {
+#})
+
+#Finally put a textInput under sliderInput and in the same row with the dateRangeInput. Name it “text”, put as label “Text Input” and as value “Some Text”. Hint: Use fluidrow, column and h4.
+
+
 library(shiny)
 shinyUI(fluidPage(
   titlePanel("Shiny App"),
   
   sidebarLayout(
-    sidebarPanel(
-      h2("Menu"),
-      br(),
-      fluidRow(
-        column(6,
-        h4("ActionButton"),
-        actionButton("per", label = "Perform")),
-        column(6,
-        h4("Help Text"),
-        helpText("Just For Help"))),
-      br(),
-      fluidRow(
-        column(6,
-        h4("SubmitButton"),
-        submitButton("Submit")),
-        column(6,
-               numericInput("numer",
-                            label = h4("Numeric Input"),
-                            value = 10))),
-      fluidRow(
-        column(6,
-      h4("Single Checkbox"),
-      checkboxInput("checkbox", label = "Choice A", value = TRUE)),
-      column(6,
-             radioButtons("radiobuttons", label = h4("Radio Buttons"),
-                          choices = list("Choice 1"=1,
-                                         "Choice 2"=2,
-                                         "Choice 3"=3),
-                          selected=2))),
-      fluidRow(
-        column(6, 
-      checkboxGroupInput("checkGroup",
-                         label = h4("Checkbox Group"),
-                         choices = list("Choice 1"=1,
-                                        "Choice 2"=2,
-                                        "Choice 3"=3),
-                         selected = 2)),
-      column(6,
-             selectInput("select", label = h4("Select Box"),
-                         choices = list("Choice 1"=1,
-                                        "Choice 2"=2),
-                         selected = 1))),
-      fluidRow(
-        column(6,
-      dateInput("date", 
-                label = h4("Date Input"),
-                value = "2017-01-11")),
-      column(6,
-             sliderInput("slider1", label = h4("Sliders"), min = 0, max = 100, value = c(10,90))),
-      dateRangeInput("date",
-                     label = h4("Date Range")),
-      fileInput("file",
-                label = h4("File Input"))),
-    
+    sidebarPanel(h2("Menu"),
+                 br(),
+                 fluidRow(
+                   column(6,
+                          h4("Actionbutton"),
+                          actionButton("per", label = "Perform")),
+                   column(6,
+                          h4("Help Text"),
+                          helpText("Just for help"))),
+                 br(),
+                 fluidRow(
+                   column(6,
+                          h4("Submitbutton"),
+                          submitButton("Submit")),
+                   column(6,
+                          numericInput("numer",
+                                       label = h4("Numeric Input"),
+                                       value = 10))),
+                 fluidRow(
+                   column(6,
+                          h4("Single Checkbox"),
+                          checkboxInput("checkbox", label = "Choice A", value = TRUE)),
+                   column(6,
+                          radioButtons("radio", label = h4("Radio Buttons"),
+                                       choices = list("Choice 1" = 1, "Choice 2" = 2),
+                                       selected = 2))),
+                 fluidRow(
+                   column(6,
+                          checkboxGroupInput("checkGroup",
+                                             label = h4("Checkbox group"),
+                                             choices = list("Choice 1" = 1,
+                                                            "Choice 2" = 2, "Choice 3" = 3),
+                                             selected = 2)),
+                   
+                   column(6,
+                          selectInput("select", label = h4("Select Box"),
+                                      choices = list("Choice 1" = 1, "Choice 2" = 2
+                                      ), selected = 1))),
+                 fluidRow(
+                   column(6,
+                          dateInput("date",
+                                    label = h4("Date input"),
+                                    value = "2016-12-01")),
+                   column(6,
+                          sliderInput("slider1", label = h4("Sliders"),
+                                      min = 0, max = 100, value = c(10,90)))),
+                 fluidRow(
+                   column(6,
+                 dateRangeInput("dates", label = h4("Date Range"))),
+                 column(6,
+                        textInput("text", label = h4("Text Input"), value = "Some Text"))),
+                 fileInput("file", label = h4("File Input"))),
     mainPanel(h1("Main"),
               img(src = "petal.jpg", height = 150, width = 200),
               br(),
               br(),
-              p("This famous (Fisher’s or Anderson’s)",a("iris", href="http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/iris.html"),"data set gives the measurements in centimeters of the variables sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris. The species are ",strong("Iris setosa"), strong("versicolor"),"and", strong("virginica.")),
+              p("This famous (Fisher's or Anderson's) ", a("iris",href="http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/iris.html"), "data set gives the measurements in centimeters of the variables sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris. The species are ",strong( "Iris setosa,"),strong( "versicolor"), "and", strong("virginica.")),
               br(),
               h2("Analysis")
+              
     )
   )
-  
 ))
+
+
+
 
